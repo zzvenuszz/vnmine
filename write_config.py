da9b83282e1f12e90049211312e2cba30febeb6b
zzvenuszz <zzvenuszz@gmail.com>
2026-06-28 07:17:07 +0000

    chore: dọn dẹp file tạm không cần thiết

--- write_config.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-with open("src/main/java/com/vnmine/cultivation/MeditationConfig.java","w") as f:
-lines.append("package com.vnmine.cultivation;")
-lines=["OK"]; open("o.txt","w").write(str(len(lines)))
-lines.append("")
-lines = []
-    f.write("\n".join(lines))
